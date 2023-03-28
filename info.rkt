21e86cf7d834e5079bef7c4d83533e92c39bac3b
davidelettieri <davide.lettieri@gmail.com>
2023-03-28 20:03:06 +0200

    update deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racket-lox")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "parser-tools-lib"))
+(define deps '("base" "parser-tools-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/racket-lox.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
