e04315b6885a5f7a18f904a527a1886d8646ab8b
davidelettieri <davide.lettieri@gmail.com>
2023-03-30 20:34:29 +0200

    add tests

---
+++ test.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(module+ test
+  (require rackunit
+           parser-tools/lex
+           "lexer.rkt"
+           "parser.rkt"
+           "semantics.rkt")
+
+  (define (parse-string s)
+    (let ([in (open-input-string s)])
+      (the-parser (lambda () (lox-lexer in)))))
+
+  (test-case
+   "Tokens are recognized correctly"
+   (let ([in (open-input-string "()+-*/ 123\"test\" identifier")])
+     (check-eqv? (position-token-token (lox-lexer in)) 'LEFT_PAREN)
+     (check-eqv? (position-token-token (lox-lexer in)) 'RIGHT_PAREN)
+     (check-eqv? (position-token-token (lox-lexer in)) 'PLUS)
+     (check-eqv? (position-token-token (lox-lexer in)) 'MINUS)
+     (check-eqv? (position-token-token (lox-lexer in)) 'STAR)
+     (check-eqv? (position-token-token (lox-lexer in)) 'SLASH)
+     (let ([t (lox-lexer in)])
+       (check-eqv? (token-name (position-token-token t)) 'NUM)
+       (check-eqv? (token-value (position-token-token t)) 123))
+     (let ([t (lox-lexer in)])
+       (check-eqv? (token-name (position-token-token t)) 'STRING)
+       (check-equal? (token-value (position-token-token t)) "test"))
+     (let ([t (lox-lexer in)])
+       (check-eqv? (token-name (position-token-token t)) 'IDENTIFIER)
+       (check-equal? (token-value (position-token-token t)) "identifier"))))
+  (test-case
+   "Simple expression are parsed correctly"
+   (let ([sources (list
+                   '("2+2" . (add 2 2))
+                   '("2-2" . (subtract 2 2))
+                   '("2*3" . (multiply 2 3))
+                   '("2/3" . (divide 2 3))
+                   '("2+3*2" . (add 2 (multiply 3 2))))])
+     (for-each
+      (lambda (el) (check-equal? (syntax->datum (parse-string (car el))) (cdr el))) sources))))
