556561a09af10248fd1da4c4c6020f7c9e9ed1cb
davidelettieri <davide.lettieri@gmail.com>
2023-04-09 09:58:19 +0200

    latest

--- parser.rkt
+++ parser.rkt
@@ -1,9 +1,16 @@
-#lang racket/base
+#lang racket
 
 (require parser-tools/yacc
          parser-tools/lex
          "lexer.rkt")
 
+
+(define-syntax-rule (while test body ...)
+  (local [(define (loop)
+            (when test
+              body ...
+              (loop)))]
+    (loop)))
 
 ; (struct srcloc (source line column position span))
 (define (positions->srcloc start-pos end-pos)
@@ -18,28 +25,55 @@
 (define (position-token->syntax val start-pos end-pos)
   (datum->syntax #f val (positions->srcloc start-pos end-pos)))
 
-(define the-parser
-  (parser
-   [start expr]
-   [end EOF]
-   [error void]
-   [src-pos]
-   [tokens basic-tokens punct-tokens]
-   ; precs clause go from lowest to highest priority
-   ; tokens at the same level share the same priority
-   [precs (left PLUS MINUS)
-          (left STAR SLASH)]
-   [grammar
-          ; the start position of a parenthesized expression is the start position of the open '('
-    [expr [(LEFT_PAREN expr RIGHT_PAREN) (position-token->syntax $2 $1-start-pos $3-end-pos)] 
-          ; the start position of a number is the start position of the number token
-          [(NUM) (position-token->syntax $1 $1-start-pos $1-end-pos)]
-          ; the start position of all binary expression is the start position of the first expression
-          ; the end position is the end position of the second expression
-          [(expr STAR expr) (position-token->syntax `(multiply ,$1 ,$3) $1-start-pos $3-end-pos)]
-          [(expr SLASH expr) (position-token->syntax `(divide ,$1 ,$3) $1-start-pos $3-end-pos)]
-          [(expr MINUS expr) (position-token->syntax `(subtract ,$1 ,$3) $1-start-pos $3-end-pos)]
-          [(expr PLUS expr) (position-token->syntax `(add ,$1 ,$3) $1-start-pos $3-end-pos)]]
-    ]))
+(define (get-tokens l)
+  (define (get-tokens-inner source)
+    (let [(t (l))] (if (eof-object? t) source (cons t (get-tokens-inner)))))
+  (reverse (get-tokens-inner '())))
 
-(provide the-parser)
+(define (lox-parser tokens)
+  (define current 0)
+  (define (declaration) #f)
+  (define (peek) (list-ref current tokens))
+  (define (is-at-end?) (eof-object? (peek)))
+  (define (parse)
+    (define statements '())
+    (while (not (is-at-end?))
+      (set! statements (cons (declaration) statements)))
+    statements)
+  (parse))
+
+(provide lox-parser get-tokens)
+
+
+; (define the-parser
+;   (parser
+;    [start program]
+;    [end EOF]
+;    [error void]
+;    [src-pos]
+;    [tokens basic-tokens punct-tokens]
+;    ; precs clause go from lowest to highest priority
+;    ; tokens at the same level share the same priority
+;    [precs (left PLUS MINUS)
+;           (left STAR SLASH)]
+;    [grammar
+;     [program [() '()]
+;              [(declaration program) (cons $1 $2)]]
+;     [declaration [(classDecl) $1]]
+;                  [(funDecl) $1]
+;                  [(varDecl) $1]
+;                  [(statement) $1]
+;     [functions [() '()]
+;                [(function functions) (cons $1 $2)]]
+;     [classDecl [(CLASS IDENTIFIER LEFT_BRACE functions RIGHT_BRACE)]]
+;           ; the start position of a parenthesized expression is the start position of the open '('
+;     [expr [(LEFT_PAREN expr RIGHT_PAREN) (position-token->syntax $2 $1-start-pos $3-end-pos)] 
+;           ; the start position of a number is the start position of the number token
+;           [(NUM) (position-token->syntax $1 $1-start-pos $1-end-pos)]
+;           ; the start position of all binary expression is the start position of the first expression
+;           ; the end position is the end position of the second expression
+;           [(expr STAR expr) (position-token->syntax `(multiply ,$1 ,$3) $1-start-pos $3-end-pos)]
+;           [(expr SLASH expr) (position-token->syntax `(divide ,$1 ,$3) $1-start-pos $3-end-pos)]
+;           [(expr MINUS expr) (position-token->syntax `(subtract ,$1 ,$3) $1-start-pos $3-end-pos)]
+;           [(expr PLUS expr) (position-token->syntax `(add ,$1 ,$3) $1-start-pos $3-end-pos)]]
+;     ]))
--- semantics.rkt
+++ semantics.rkt
@@ -5,7 +5,21 @@
 (define multiply *)
 (define divide /)
 
+(define-syntax-rule (binary left operator right)
+    (operator left right))
+
+; (struct (lox-call callee paren arguments))
+; (struct (lox-get obj name))
+; (struct (lox-grouping expr))
+; (struct (lox-literal value))
+; (struct (lox-logical left operator right))
+; (struct (lox-unary operator right))
+; (struct (lox-set obj name value))
+; (struct (lox-super))
+
 (provide subtract
          add
          multiply
-         divide)
+         divide
+         binary)
+
