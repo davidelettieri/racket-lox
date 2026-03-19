f1453b16c375cdd553202fd9bdd5c4fc869d9df7
davidelettieri <davide.lettieri@gmail.com>
2026-03-19 12:52:40 +0100

    chore: remove unused code

--- lang/helpers.rkt
+++ lang/helpers.rkt
@@ -10,14 +10,4 @@
            body ...
            (loop)))]))
 
-(define-syntax (do stx)
-  (syntax-parse stx
-    [(_ body:expr ... (~literal while) condition:expr)
-     #'(let loop ()
-         body
-         ...
-         (when condition
-           (loop)))]))
-
-(provide while
-         do)
+(provide while)
